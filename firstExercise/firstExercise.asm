424171f2295f5c18dbc2a3b291a1c5d11987ecfe
mozerpol <mozerpol@protonmail.com>
2020-11-22 21:33:26 +0100

    Fixed code and added comments

--- firstExercise/firstExercise.asm
+++ firstExercise/firstExercise.asm
@@ -6,25 +6,26 @@ init:
     out     DDRC, r16
     
     ldi     r16, 0xFF
-    out     PORTC, r16 ; pull internally to logical 1
+    out     PORTC, r16 ; pull all PORTC internally to logical 1
 main:
-    in      r16, PINC
-    andi    r16, 0x07 ; 0x07 = 0b00000111
-    cpi     r16, 0x05 ; 0b11111110
+    in      r16, PINC ; read the value from PORTC
+    andi    r16, 0x07 ; 0x07 = 0b00000111 this will keep only MSB: PC0, PC1, PC2
+    cpi     r16, 0x05 ; 0x05 = 0b11111101 compare R16 with 0x05. If PC1 = GND,
+                      ; then jump to SW1 label
     breq    SW1
     
     in      r16, PINC
-    andi    r16, 0x07 ; 0x07 =  0b00000111
-    cpi     r16, 0x06 ; 0b11111101
-    breq    SW2
+    andi    r16, 0x07
+    cpi     r16, 0x06 ; 0b11111110
+    breq    SW0
     
     rjmp    main
 SW1:
-    ldi     r16, 0xff
+    ldi     r16, 0xFF ; Turn off all LEDs
     out     PORTC, r16 
     rjmp    main
-SW2:
-    ldi     r16, 0
+SW0:
+    ldi     r16, 0 ; Turn on all LEDs
     out     PORTC, r16 
     rjmp    main    
     
