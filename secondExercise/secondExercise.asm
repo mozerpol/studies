8cc13dbb8f91c99b5143da1b5a1b241432d6912f
mozerpol <mozerpol@protonmail.com>
2020-11-23 21:11:27 +0100

    Add definitions of numb in 7seg display

--- secondExercise/secondExercise.asm
+++ secondExercise/secondExercise.asm
@@ -1,30 +1,31 @@
 .include "./m328Pdef.inc" ; directive for pin and register definitions
 
+.EQU    SEG_ONE = 0x7B ; 0b01111011
+.EQU    SEG_TWO = 0x34 ; 0b00110100
+.EQU    SEG_THREE = 0x31 ; 0b00110001
+.EQU    SEG_FOUR = 0x63 ; 0b01100011
+.EQU    SEG_FIVE = 0x81 ; 0b10000001
+.EQU    SEG_SIX = 0x80 ; 0b10000000
+
 init:
-    ldi     r16, 0b11111110 ; set PC0 as input and rest of pins as output
-    out     DDRC, r16
+    ldi     r16, 0xFF ; set PORTD as output
+    out     DDRD, r16
     
     ldi     r16, 0xFF
-    out     PORTC, r16 ; pull all PORTC internally to logical 1
+    out     PORTD, r16 ; pull all PORTC internally to logical 1, so turn off LED
 main:
-    in      r16, PINC ; read the value from PORTC
-    andi    r16, 0x07 ; 0x07 = 0b00000001 this will keep only MSB: PC0, PC1, PC2
-    cpi     r16, 0x06 ; 0x06 = 0b00000110 compare R16 with 0x05. If PC1 = GND,
-                      ; then jump to SW0 label
-    breq    SW0   
+    ldi     r17, 0b10000000
+    out     PORTD, r17
     rjmp    main
     
 SW0:
-    call    delay
-    ldi     r16, 0 ; Turn on all LEDs
-    out     PORTC, r16 
     rjmp    main
 
 delay:    
-    ldi     r16, 16
-    ldi     r18, 255
+    ldi     r16, 50
+    ldi     r18, 18
     loop_2:
-        ldi     R17, 100
+        ldi     R17, 17
     loop_1:   
         dec     R18
         brne    loop_1
