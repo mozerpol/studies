7717ffa17ad586be25dd1553176e9cdfbf3bffdb
mozerpol <mozerpol@protonmail.com>
2020-11-29 00:07:58 +0100

    Delete unnecessary files

--- detectPressButton/detectPressButton.asm
+++
@@ -1,7 +0,0 @@
-.include "./m328Pdef.inc"
-
-ldi     r16, 0 
-out     DDRC, r16 ; set DDRC as a input
-
-ldi     r16, 0
-out     PORTC, r16 ; pull internally PORTC to logic low.
--- turnOnLED/turnOnLED.asm
+++
@@ -1,8 +0,0 @@
-.include "./m328Pdef.inc"
-
-ldi     r16, 0xFF 
-out     DDRC, r16 ; set DDRC as a output
-
-ldi     r16, 0b00100000 
-out     PORTC, r16 ; pull internally PC5 to logic high. The rest off the pins
-; pull internally to logic low.  
