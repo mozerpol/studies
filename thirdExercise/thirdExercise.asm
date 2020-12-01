2b36c66ab6cbd5a46cb2095866c4067b6524d7c1
mozerpol <mozerpol@protonmail.com>
2020-12-01 17:40:49 +0100

    Create second table for 7seg display

--- thirdExercise/thirdExercise.asm
+++ thirdExercise/thirdExercise.asm
@@ -1,17 +1,27 @@
 .include "./m328Pdef.inc" ; directive for pin and register definitions
 
-.EQU    SEG_ZERO = 0x08 ; 0x08 = 0b00001000
-.EQU    SEG_ONE = 0x7B ; 0x7B = 0b01111011
-.EQU    SEG_TWO = 0x34 ; 0x34 = 0b00110100
-.EQU    SEG_THREE = 0x31 ; 0x31 = 0b00110001
-.EQU    SEG_FOUR = 0x63 ; 0x63 = 0b01100011
-.EQU    SEG_FIVE = 0x81 ; 0x81 = 0b10000001
-.EQU    SEG_SIX = 0xA0 ; 0xA0 = 0b10100000
-.EQU    SEG_SEVEN = 0x3B ; 0x3B = 0b00111011
-.EQU    SEG_EIGHT = 0x00 ; 0x00 = 0b00000000
-.EQU    SEG_NINE = 0x01 ; 0x01 = 0b00000001
-.EQU    FREE = 0xDD ; 0xDD = 0b11011101
-.EQU    FULL = 0xEF ; 0xEF = 0b11101111
+.EQU    SEG_ZERO1 = 0x08 ; 0x08 = 0b00001000
+.EQU    SEG_ONE1 = 0x7B ; 0b01111011
+.EQU    SEG_TWO1 = 0x34 ; 0b00110100
+.EQU    SEG_THREE1 = 0x31 ; 0b00110001
+.EQU    SEG_FOUR1 = 0x63 ; 0b01100011
+.EQU    SEG_FIVE1 = 0x81 ; 0b10000001
+.EQU    SEG_SIX1 = 0xA0 ; 0b10100000
+.EQU    SEG_SEVEN1 = 0x3B ; 0b00111011
+.EQU    SEG_EIGHT1 = 0x00 ; 0b00000000
+.EQU    SEG_NINE1 = 0x01 ; 0b00000001
+
+.EQU    SEG_ZERO2 = 0x10 ; 0x08 = 0b00010000
+.EQU    SEG_ONE2 = 0xDD ; 0b11011101
+.EQU    SEG_TWO2 = 0x29 ; 0b00101001
+.EQU    SEG_THREE2 = 0x89 ; 0b10001001
+.EQU    SEG_FOUR2 = 0xC5 ; 0b11000101
+.EQU    SEG_FIVE2 = 0x83 ; 0b10000011
+.EQU    SEG_SIX2 = 0x03 ; 0b00000011
+.EQU    SEG_SEVEN2 = 0xD9 ; 0b11011001
+.EQU    SEG_EIGHT2 = 0x00 ; 0b00000000
+.EQU    SEG_NINE2 = 0xC1 ; 0b11000001
+
 .EQU	numB = 10 ; number of bytes in array
 .DEF	tmp	= r21 ; define temp register
 .DEF	loopCt	= r22 ; define loop count register
@@ -25,27 +35,27 @@ sArr:
     ldi	    XL, LOW(sArr)		; initialize X pointer
     ldi	    XH, HIGH(sArr)		; to SRAM array address
 
-    ldi	    ZL, LOW(2*numbers7seg)		; initialize Z pointer
-    ldi	    ZH, HIGH(2*numbers7seg)		; to pmem array address
+    ldi	    ZL, LOW(2*numbers7seg1)		; initialize Z pointer
+    ldi	    ZH, HIGH(2*numbers7seg1)		; to pmem array address
 
     ldi	    loopCt, numB		; initialize loop count to number of bytes
 
 init:
-    ldi     r20, 0xff ; set PORTD as output where is 7seg display
+    ldi     r20, 0xff ; set PORTD as output where is 7seg display1
     out     DDRD, r20
-    ldi     r20, SEG_ZERO
+    out     DDRB, r20 ; set PORTB as output where is 7seg display2
+    ldi     r20, SEG_ZERO1
 	out     PORTD, r20
-	
-	ldi     r20, 0xff ; set PORTB as output where is 7seg display
-    out     DDRB, r20
+    ldi     r20, SEG_NINE2   
 	out     PORTB, r20
+
 	
 	ldi     r16, 0b11111000 ; set PC0, PC1, PC2 as input and rest of pins as
     out     DDRC, r16       ; output
     ldi     r16, 0xFF
     out     PORTC, r16 ; pull all PORTC internally to logical 1
 	
-arrLp:	     
+main:	     
     in      r16, PINC ; read the value from PORTC
     andi    r16, 0x07 ; 0x07 = 0b00000111 this will keep only MSB: PC0, PC1, PC2
     cpi     r16, 0x05 ; 0x05 = 0b11111101 compare R16 with 0x05. If PC1 = GND,          
@@ -61,40 +71,34 @@ arrLp:
     cpi     XL, 0x0A ; 0b00001001
     breq    offAccessBarrier    
     
-    rjmp    arrLp
+    rjmp    main
 
 onAccessBarrier:
-    ldi     r16, FREE
-    out     PORTB, r16 ; pull all PORTC internally to logical 1
-    rjmp    arrLp 
+    ldi     r16, 0b11110111
+    out     PORTC, r16 ; pull all PORTC internally to logical 1
+    rjmp    main 
 offAccessBarrier:
-    ldi     r16, FULL
-    out     PORTB, r16 ; pull all PORTC internally to logical 1
-    rjmp    arrLp 
+    ldi     r16, 0b11101111
+    out     PORTC, r16 ; pull all PORTC internally to logical 1
+    rjmp    main 
     
 nextValue:
     lpm	    tmp, Z+			; load value from pmem array
 	out     PORTD, tmp
+	out     PORTB, tmp
 	call    delay
 	st	    X+, tmp			; store value to SRAM array
-    rjmp    arrLp
+    rjmp    main
 
 previousValue:
     subi    ZL, 1
     lpm	    tmp, Z			; load value from pmem array
 	out     PORTD, tmp
+	out     PORTB, tmp
 	call    delay
 	st	    X+, tmp			; store value to SRAM array
-    rjmp    arrLp
-
-
- 
-; defining constants in program memory    
-numbers7seg:    
-    .db     SEG_ZERO, SEG_ONE, SEG_TWO, SEG_THREE, SEG_FOUR, SEG_FIVE
-    .db     SEG_SIX, SEG_SEVEN, SEG_EIGHT, SEG_NINE
-    
-    
+    rjmp    main
+   
  ; -------- delay function - about 1 sek -------- 
 delay:    
     ldi     r16, 20
@@ -114,8 +118,13 @@ delay:
         brne    loop_2
     ret ; return form subroutine   
     
-    
-    
+ ; defining constants in program memory    
+numbers7seg1:    
+    .db     SEG_ZERO1, SEG_ONE1, SEG_TWO1, SEG_THREE1, SEG_FOUR1, SEG_FIVE1
+    .db     SEG_SIX1, SEG_SEVEN1, SEG_EIGHT1, SEG_NINE1   
+numbers7seg2:    
+    .db     SEG_NINE2, SEG_EIGHT2, SEG_SEVEN2, SEG_SIX2, SEG_FIVE2, SEG_FOUR2
+    .db     SEG_THREE2, SEG_TWO2, SEG_ONE2, SEG_ZERO2       
     
     
     
