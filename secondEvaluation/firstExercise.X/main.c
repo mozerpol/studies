3fba34277b23db2d3ce4e354875b8efa55b099cd
mozerpol <mozerpol@protonmail.com>
2020-12-25 23:03:40 +0100

    Simple turn on LED on 7seg

--- secondEvaluation/firstExercise.X/main.c
+++ secondEvaluation/firstExercise.X/main.c
@@ -1,10 +1,15 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <avr/io.h>
+#include <util/delay.h>
 
-
-int main(int argc, char** argv) 
+int main(void)
 {
-
-    return (EXIT_SUCCESS);
+    DDRC |= (1<<PC4); // PC4 as output
+    PORTC &= ~(1<<PC4); // Pull internally PC4 to gnd
+    
+    DDRD |= (1<<PD5)|(1<<PD6); // PD5, PD6 as output
+    PORTD |= (1<<PD5)|(1<<PD6); // Pull internally PD5 and PD6 to vcc
+    
+    while(1);
+    
+    return 0;
 }
-
