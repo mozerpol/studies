e7868f9a0e9d28e039cbc861bef1c0e5eb1e08cb
mozerpol <mozerpol@protonmail.com>
2020-12-26 20:24:07 +0100

    Add comments for 7seg files

--- secondEvaluation/firstExercise.X/sevenSegment.c
+++ secondEvaluation/firstExercise.X/sevenSegment.c
@@ -6,19 +6,19 @@ uint8_t selectDisplay(uint8_t whichDisp)
     {
         case 1:
             DDRC = (1<<FIRST_7SEG);// PC4 as output 
-            PORTC &= ~(1<<whichDisp); // Pull internally PC4 to GND
+            PORTC &= ~(1<<whichDisp); // Pull internally PC0 to GND
             break;
         case 2:
-            DDRC = (1<<SECOND_7SEG); // PC4 as output 
-            PORTC &= ~(1<<whichDisp); // Pull internally PC4 to GND
+            DDRC = (1<<SECOND_7SEG);
+            PORTC &= ~(1<<whichDisp);
             break;
         case 3:
-            DDRC = (1<<THIRD_7SEG); // PC4 as output 
-            PORTC &= ~(1<<whichDisp); // Pull internally PC4 to GND
+            DDRC = (1<<THIRD_7SEG);
+            PORTC &= ~(1<<whichDisp);
             break;
         case 4:
-            DDRC = (1<<FOURTH_7SEG); // PC4 as output 
-            PORTC &= ~(1<<whichDisp); // Pull internally PC4 to GND
+            DDRC = (1<<FOURTH_7SEG);
+            PORTC &= ~(1<<whichDisp);
             break;            
     }
     return 0;
@@ -26,11 +26,13 @@ uint8_t selectDisplay(uint8_t whichDisp)
 
 uint8_t selectNumber(uint8_t whichNumber)
 {
-    DDRB |= (1<<PB0);
-    DDRD |= 0b11111111;
+    DDRB |= (1<<PB0); // 7-seg display is divided between PORTB and PORTD, one
+                      // pin is connected to PB0, so we must set PB0 as output.
+    DDRD |= 0b11111111; // Almost all 7-seg pins are connected to PORTD, so we
+                        // must set PORTD as output.
     switch(whichNumber)
     {
-        case 0:
+        case 0: // number 0 is some pins from PORTD and one pin from PORTB
             PORTD = ZERO_7SEG;
             PORTB = (1<<PB0);
             break;
@@ -71,6 +73,7 @@ uint8_t selectNumber(uint8_t whichNumber)
     return 0;
 }
 
+/// Function for more convenience
 uint8_t displayNumber(uint8_t whichDisp, uint8_t whichNumber)
 {
     selectDisplay(whichDisp);
--- secondEvaluation/firstExercise.X/sevenSegment.h
+++ secondEvaluation/firstExercise.X/sevenSegment.h
@@ -3,6 +3,7 @@
 #include <util/delay.h>
 #include <avr/io.h>
 
+// Macros for displaying numbers on the 7 segment display
 #define ZERO_7SEG   0b10111111
 #define ONE_7SEG    0b00100100
 #define TWO_7SEG    0b11011100
@@ -13,13 +14,13 @@
 #define SEVEN_7SEG  0b10100100
 #define EIGHT_7SEG  0b11111111
 #define NINE_7SEG   0b11110100
-
+// Macros for selecting 7 segment display
 #define FIRST_7SEG  0b00000011
 #define SECOND_7SEG 0b00000000
 #define THIRD_7SEG  0b00000001
 #define FOURTH_7SEG 0b00000010
 
-uint8_t selectDisplay(uint8_t whichDisp);
-uint8_t selectNumber(uint8_t whichNumber);
-uint8_t displayNumber(uint8_t whichDisp, uint8_t whichNumber);
+uint8_t selectDisplay(uint8_t whichDisp); // return 0
+uint8_t selectNumber(uint8_t whichNumber); // return 0
+uint8_t displayNumber(uint8_t whichDisp, uint8_t whichNumber); // return 0
 
